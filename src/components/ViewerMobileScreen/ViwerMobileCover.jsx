150258508e1387ccc794534c880ecb20d2a428a1
mayar1 aljabasene <mayaraljabasene@gmail.com>
2024-03-24 22:43:23 +0300

    test

--- src/components/ViewerMobileScreen/ViwerMobileCover.jsx
+++ src/components/ViewerMobileScreen/ViwerMobileCover.jsx
@@ -31,103 +31,93 @@ function ViwerMobileCover() {
 
   return (
     <div className="viwer-mobile">
-      <div className="top-section">
-        <div className="box-gear-top">
-          <div className="viwer-number">
-            <i className="fa-solid fa-user" />
-            <span>5</span>
-          </div>
-          <TooltipMobileAction
-            classNeed={"HandelTool"}
-            title={
-              <ul className="box-mobile-toltip list-unstyled">
-                <li>
-                  <i
-                    className="fa-regular fa-user"
-                    style={{ fontSize: "15px" }}
-                  />
-                  <span>User login</span>
-                </li>
-                <li>
-                  <i
-                    className="fa-solid fa-video-slash"
-                    style={{ fontSize: "15px" }}
-                  />
-                  <span>Enable audio only</span>
-                </li>
-                <li>
-                  <i
-                    className="fa-solid fa-share"
-                    style={{ fontSize: "15px" }}
-                  />
-                  <span>Share room</span>
-                </li>
-              </ul>
-            }
-          >
-            <i className="fa-solid fa-gear"></i>
-          </TooltipMobileAction>
+      <div className="box-gear-top">
+        <div className="viwer-number">
+          <i className="fa-solid fa-user" />
+          <span>5</span>
         </div>
-        {/**<div className="fading-overlay"></div> */}
+        <TooltipMobileAction
+          classNeed={"HandelTool"}
+          title={
+            <ul className="box-mobile-toltip list-unstyled">
+              <li>
+                <i
+                  className="fa-regular fa-user"
+                  style={{ fontSize: "15px" }}
+                />
+                <span>User login</span>
+              </li>
+              <li>
+                <i
+                  className="fa-solid fa-video-slash"
+                  style={{ fontSize: "15px" }}
+                />
+                <span>Enable audio only</span>
+              </li>
+              <li>
+                <i className="fa-solid fa-share" style={{ fontSize: "15px" }} />
+                <span>Share room</span>
+              </li>
+            </ul>
+          }
+        >
+          <i className="fa-solid fa-gear"></i>
+        </TooltipMobileAction>
       </div>
+      {/**<div className="fading-overlay"></div> */}
 
-      <div className="bottom-section">
-        <div className="commints">
-          {data &&
-            data.map((e) => {
-              return (
-                <div className="meesage-box">
-                  <div className="img-box">
-                    {e.img ? <img src={img1} alt="..." /> : <span>J</span>}
-                  </div>
-                  <div className="box-title">
-                    <span className="name">{e.name}</span>
-                    <span className="message">{e.message}</span>
-                  </div>
+      <div className="commints">
+        {data &&
+          data.map((e) => {
+            return (
+              <div className="meesage-box">
+                <div className="img-box">
+                  {e.img ? <img src={img1} alt="..." /> : <span>J</span>}
                 </div>
-              );
-            })}
-        </div>
-        <form className="box-write-bottom" onSubmit={(e) => SendCommint(e)}>
-          <button className="icon-send">
-            <i
-              className={`fa-solid fa-play   ${inp && "active"}`}
-              onClick={(e) => handleChange(e)}
-            />
-          </button>
-          <input
-            type="text"
-            placeholder="Add comment"
-            value={inp}
-            onChange={(e) => setInp(e.target.value)}
-          />
-
-          <TooltipMobileAction
-            classNeed={"HandelTool"}
-            title={
-              <ul className="box-mobile-toltip list-unstyled">
-                <li>
-                  <i
-                    className="fa-solid fa-microphone"
-                    style={{ fontSize: "15px" }}
-                  />
-                  <span>Request mic</span>
-                </li>
-                <li>
-                  <i
-                    className="fa-solid fa-video"
-                    style={{ fontSize: "14px" }}
-                  />
-                  <span>Request cam</span>
-                </li>
-              </ul>
-            }
-          >
-            <i className="fa-solid fa-hand" />
-          </TooltipMobileAction>
-          <i className="fa-solid fa-heart ms-1 me-2" />
-        </form>
+                <div className="box-title">
+                  <span className="name">{e.name}</span>
+                  <span className="message">{e.message}</span>
+                </div>
+              </div>
+            );
+          })}
       </div>
+      <form className="box-write-bottom" onSubmit={(e) => SendCommint(e)}>
+        <button className="icon-send">
+          <i
+            className={`fa-solid fa-play   ${inp && "active"}`}
+            onClick={(e) => handleChange(e)}
+          />
+        </button>
+        <input
+          type="text"
+          placeholder="Add comment"
+          value={inp}
+          onChange={(e) => setInp(e.target.value)}
+        />
+
+        <TooltipMobileAction
+          classNeed={"HandelTool"}
+          title={
+            <ul className="box-mobile-toltip list-unstyled">
+              <li>
+                <i
+                  className="fa-solid fa-microphone"
+                  style={{ fontSize: "15px" }}
+                />
+                <span>Request mic</span>
+              </li>
+              <li>
+                <i className="fa-solid fa-video" style={{ fontSize: "14px" }} />
+                <span>Request cam</span>
+              </li>
+            </ul>
+          }
+        >
+          <i className="fa-solid fa-hand" />
+        </TooltipMobileAction>
+        <i className="fa-solid fa-heart ms-1 me-2" />
+      </form>
     </div>
   );
 }
